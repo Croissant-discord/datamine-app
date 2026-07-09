d9b71cd4ea453aa73cff90e967d6bea231e380ed
SanseL <sanselguh@gmail.com>
2026-07-09 17:22:42 +0700

    Add files via upload

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,16 @@
 <template>
-  <div style="width: 100%; height: 150px; background-color: #CE7F1F;"></div>
-  
-  <nav style="background-color: #1d1d1d; padding: 10px; gap: 10px; display: flex;">
-    <router-link to="/">Home</router-link>
-    <router-link to="/settings">Settings</router-link>
-  </nav>
+  <div class="main-container" style="max-width: 400px;">
+    <div class="Hero" style="">
+      180000
+    </div>
 
-  <router-view />
+    <nav style="background-color: #1d1d1d;">
+      <router-link to="/">Home</router-link>
+      <router-link to="/settings">Settings</router-link>
+    </nav>
+
+    <router-view />    
+  </div>
 </template>
 
 <style>
@@ -14,5 +18,35 @@
     margin: 0;
     padding: 0;
     background-color: #3c3c3c;
+
   }
+</style>
+
+<style scoped>
+.Hero {
+  width: 100%; 
+  height: 150px; 
+
+  font-family: 'Bungee', sans-serif;
+  color: #fff;
+  background-image: url('@/assets/hero.svg');
+  background-size: cover;
+
+  background-color: #CE7F1F; 
+  display: flex; 
+  align-items: center; 
+  justify-content: center;
+}
+
+nav {
+  background-color: #1d1d1d;
+  padding: 10px;
+  gap: 10px;
+  display: flex;
+}
+
+nav :deep(a) {
+  color: #fff;
+  text-decoration: none;
+}
 </style>
--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,19 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import process from "node:process";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+
 const host = process.env.TAURI_DEV_HOST;
 
-// https://vite.dev/config/
 export default defineConfig(() => ({
   plugins: [vue()],
-
-  // Vite options tailored for Tauri development and only applied in `tauri dev` or `tauri build`
-  //
-  // 1. prevent Vite from obscuring rust errors
+  resolve: {
+    alias: {
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
+    },
+  },
   clearScreen: false,
-  // 2. tauri expects a fixed port, fail if that port is not available
   server: {
     port: 1420,
     strictPort: true,
@@ -24,7 +26,6 @@ export default defineConfig(() => ({
         }
       : undefined,
     watch: {
-      // 3. tell Vite to ignore watching `src-tauri`
       ignored: ["**/src-tauri/**"],
     },
   },
